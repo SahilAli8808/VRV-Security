ee636f4446444a15de4747cf3315d00d81f3ba04
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-11-29 23:43:22 +0530

    feat: add CORS support and Swagger documentation setup in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,25 @@
+// index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
+const swaggerDocument = require('./swagger-output.json'); // Path to the generated Swagger JSON
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 const app = express();
-
+// Enable CORS for all origins (or restrict to a specific origin)
+app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
